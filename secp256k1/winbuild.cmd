4a69c07703cd85da18b170122e184a9a22e32797
amin63moradi <amin8moradi@yahoo.com>
2021-01-17 21:51:25 +0330

    Update winbuild.cmd

--- secp256k1/winbuild.cmd
+++ secp256k1/winbuild.cmd
@@ -14,13 +14,5 @@ nvcc -o ../miner.exe -Xcompiler "/std:c++14" -gencode arch=compute_%CUDA_COMPUTE
 conversion.cc cryptography.cc definitions.cc jsmn.c httpapi.cc ^
 mining.cu prehash.cu processing.cc request.cc easylogging++.cc autolykos.cu
 
-nvcc -o ../test.exe -Xcompiler "/std:c++14" -gencode arch=compute_%CUDA_COMPUTE_ARCH%,code=sm_%CUDA_COMPUTE_ARCH%^
- -gencode arch=compute_30,code=compute_30 -DBLOCK_DIM=%BLOCK_DIM% -DNONCES_PER_ITER=%WORKSPACE%^
- -I %OPENSSL_DIR%\include ^
- -I %LIBCURL_DIR%\include ^
- -l %LIBCURL_DIR%\builds\libcurl-vc-x64-release-dll-ipv6-sspi-winssl-obj-lib/libcurl ^
- -l %OPENSSL_DIR%\lib\libeay32 -L %OPENSSL_DIR%/lib ^
-test.cu conversion.cc cryptography.cc definitions.cc jsmn.c ^
-mining.cu prehash.cu processing.cc request.cc easylogging++.cc
 cd ..
 SET PATH=%PATH%;C:\Program Files\NVIDIA Corporation\NVSMI
